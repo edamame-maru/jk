81d77c78ca53d81203f74d0b2c8750689f17c28a
edamame-maru <so1maru.ueda@gmail.com>
2025-01-25 18:43:58 +0000

    added save

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 #include <QFont>
 #include <QShortcut>
 #include <QKeySequence>
+#include <QFileDialog>
+#include <QFile>
+#include <QTextStream>
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
@@ -15,7 +18,7 @@ int main(int argc, char *argv[]) {
     font.setPointSize(11);
     textEdit->setFont(font);
 
-    textEdit->setPlaceholderText("Press Ctrl+Q to exit.");
+    textEdit->setPlaceholderText("Press Ctrl+Q to exit; Ctrl+S to write.");
 
     window.setCentralWidget(textEdit);
 
@@ -23,9 +26,23 @@ int main(int argc, char *argv[]) {
     window.resize(800, 600);
     window.show();
 
+    // Ctrl+Q to quit.
     new QShortcut(QKeySequence(Qt::CTRL + Qt::Key_Q), &window, [&]() {
 	app.quit();
     });
+
+    // Ctrl+S to save.
+    new QShortcut(QKeySequence(Qt::CTRL + Qt::Key_S), &window, [&]() {
+        QString fileName = QFileDialog::getSaveFileName(&window, "Save File", "", "Text Files (*.txt);;All Files (*)");
+        if (!fileName.isEmpty()) {
+            QFile file(fileName);
+            if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+                QTextStream out(&file);
+                out << textEdit->toPlainText();
+                file.close();
+            }
+        }
+    }); 
 
     return app.exec();
 }
