b6771a869894df476644c5b1353ee2578350015a
edamame-maru <so1maru.ueda@gmail.com>
2025-01-25 18:29:54 +0000

    added core

--- main.cpp
+++ main.cpp
@@ -1,12 +1,32 @@
-#include "jk.h"
-
 #include <QApplication>
+#include <QMainWindow>
+#include <QTextEdit>
+#include <QFont>
+#include <QShortcut>
+#include <QKeySequence>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    jk w;
-    w.setWindowIcon(QIcon()); // Clear the icon
-    w.show();
-    return a.exec();
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    QMainWindow window;
+    QTextEdit *textEdit = new QTextEdit(&window);
+
+    QFont font = textEdit->font();
+    font.setPointSize(11);
+    textEdit->setFont(font);
+
+    textEdit->setPlaceholderText("Press Ctrl+Q to exit.");
+
+    window.setCentralWidget(textEdit);
+
+    window.setWindowTitle("jk");
+    window.resize(800, 600);
+    window.show();
+
+    new QShortcut(QKeySequence(Qt::CTRL + Qt::Key_Q), &window, [&]() {
+	app.quit();
+    });
+
+    return app.exec();
 }
+
