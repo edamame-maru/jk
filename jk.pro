ffd922881ce4261cf044cdbf4996e909cd8cdee1
edamame-maru <so1maru.ueda@gmail.com>
2025-01-26 13:37:24 +0000

    make

--- jk.pro
+++ jk.pro
@@ -1,4 +1,7 @@
-QT += widgets
-CONFIG += c++17
-
+TEMPLATE = app
+TARGET = jk
+QT += core gui widgets
 SOURCES += main.cpp
+DESTDIR = /usr/local/bin
+install.path = $$DESTDIR
+INSTALLS += install
--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char *argv[]) {
     font.setPointSize(11);
     textEdit->setFont(font);
 
-    textEdit->setPlaceholderText("Press Ctrl+Q to exit; Ctrl+S to write.");
+    textEdit->setPlaceholderText("Press Ctrl+Q to exit; Ctrl+S to save; Ctrl+O to open.");
 
     window.setCentralWidget(textEdit);
 
@@ -26,24 +26,54 @@ int main(int argc, char *argv[]) {
     window.resize(800, 600);
     window.show();
 
+    // To keep track of the current file path
+    QString currentFilePath;
+
     // Ctrl+Q to quit.
     new QShortcut(QKeySequence(Qt::CTRL + Qt::Key_Q), &window, [&]() {
-	app.quit();
+        app.quit();
+    });
+
+    // Ctrl+O to open a file.
+    new QShortcut(QKeySequence(Qt::CTRL + Qt::Key_O), &window, [&]() {
+        QString fileName = QFileDialog::getOpenFileName(&window, "Open File", "", "All Files (*)");
+        if (!fileName.isEmpty()) {
+            QFile file(fileName);
+            if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+                QTextStream in(&file);
+                textEdit->setPlainText(in.readAll());
+                file.close();
+                currentFilePath = fileName; // Save the current file path
+            } else {
+                textEdit->setPlainText("Could not open the file.");
+            }
+        }
     });
 
     // Ctrl+S to save.
     new QShortcut(QKeySequence(Qt::CTRL + Qt::Key_S), &window, [&]() {
-        QString fileName = QFileDialog::getSaveFileName(&window, "Save File", "", "Text Files (*.txt);;All Files (*)");
-        if (!fileName.isEmpty()) {
-            QFile file(fileName);
+        if (currentFilePath.isEmpty()) {
+            // If no file is opened, prompt the user to save it as a new file.
+            QString fileName = QFileDialog::getSaveFileName(&window, "Save File", "", "All Files (*)");
+            if (!fileName.isEmpty()) {
+                QFile file(fileName);
+                if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+                    QTextStream out(&file);
+                    out << textEdit->toPlainText();
+                    file.close();
+                    currentFilePath = fileName; // Update the current file path
+                }
+            }
+        } else {
+            // Save to the currently opened file.
+            QFile file(currentFilePath);
             if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
                 QTextStream out(&file);
                 out << textEdit->toPlainText();
                 file.close();
             }
         }
-    }); 
+    });
 
     return app.exec();
 }
-
